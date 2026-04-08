bde93d8554033d5aeabe1dc385b497568c42f768
kamikazerp10-blip <kamikazerp10@gmail.com>
2026-04-08 13:44:33 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Bot está online!');
+});
+
+app.listen(3000, () => {
+    console.log('Servidor web rodando');
+});
+
 const { 
   Client, GatewayIntentBits, 
   ActionRowBuilder, ButtonBuilder, ButtonStyle,
